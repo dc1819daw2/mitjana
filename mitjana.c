f294686792700f359afbac7d229bbdf431fb96c6
dc1718daw2 <dc1718daw2@gamil>
2019-01-14 13:26:42 +0100

    Commit 5 del projecte mitjana

--- mitjana.c
+++ mitjana.c
@@ -7,7 +7,7 @@ int main() {
 	int numero_repeticions=0;
 	char consulta='s';
 	
-	while(consulta=='s') {
+	while(consulta=='y') {
 		printf("Dòna la següent dada: ");
 		scanf("%f",&nou_valor);
 		suma=suma+nou_valor;
